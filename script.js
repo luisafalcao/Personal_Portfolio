b915208bc6ae321a7e4a35c06c3bdabfad3bf132
Luisa Falcao <luisafalcaovaz@gmail.com>
2022-05-31 19:42:43 -0300

    work on scroll

--- script.js
+++ script.js
@@ -10,5 +10,44 @@ const expand = () => {
 
 projectPanel.addEventListener('click', expand)
 
+// scroll
+let scrollPosition = window.scrollY
 
-// observer
+const sectionOne = document.querySelector("#about")
+const sectionTwo = document.querySelector("#projects")
+const sectionThree = document.querySelector("#skills")
+const sectionFour = document.querySelector("#contact")
+
+const sectionOneHeight = sectionOne.offsetHeight
+const sectionTwoHeight = sectionTwo.offsetHeight
+const sectionThreeHeight = sectionThree.offsetHeight
+const sectionFourHeight = sectionFour.offsetHeight
+
+const nav = document.querySelector("nav")
+
+const addClassOnScroll = () => {
+    sectionOne.classList.add("stacking-slide")
+    sectionTwo.classList.add("stacking-slide")
+    sectionThree.classList.add("stacking-slide")
+    sectionFour.classList.add("stacking-slide")
+}
+
+const removeClassOnScroll = () => {
+    sectionOne.classList.remove("stacking-slide")
+    sectionTwo.classList.remove("stacking-slide")
+    sectionThree.classList.remove("stacking-slide")
+    sectionFour.classList.remove("stacking-slide")
+}
+
+window.addEventListener('scroll', function() { 
+    scrollPosition = window.scrollY;
+
+    if (scrollPosition >= sectionOneHeight) { 
+        addClassOnScroll() 
+    }
+    else { 
+        removeClassOnScroll() 
+    }
+    // console.log(scrollPosition)
+    // console.log(sectionOneHeight)
+});
